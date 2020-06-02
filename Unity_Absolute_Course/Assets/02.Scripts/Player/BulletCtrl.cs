fe91f40e3fde77ce20c5443c535ebf430b6af924
tkd14gus <qkrtkdgus78@gmail.com>
2020-06-02 17:37:55 +0900

    절대강좌 06
    적 캐릭터 제작 : 적 캐릭터를 제작하면서 주인공과 다르게 AI를 만들고, 다른 방식으로 임팩트를 입히는 등 04 주인공 캐릭터 제작과 비슷하지만 다른 방식으로 캐릭터를 제작하는 것을 따라하며 공부하였다.

---
+++ Unity_Absolute_Course/Assets/02.Scripts/Player/BulletCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    //총알 파괴력
+    public float damage = 20.0f;
+    //총알 발사 속도
+    public float speed = 1000.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
