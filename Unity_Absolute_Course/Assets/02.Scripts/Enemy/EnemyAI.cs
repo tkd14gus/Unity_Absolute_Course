804d25bc7830091f13136eb669ff416468ab7e3c
tkd14gus <qkrtkdgus78@gmail.com>
2020-06-05 15:35:37 +0900

    절대강좌 12
    레이캐스트 활용 : 레이캐스트를 이용하여 적의 시야, 카메라 조정 등을 따라하며 공부하였다.

--- Unity_Absolute_Course/Assets/02.Scripts/Enemy/EnemyAI.cs
+++ Unity_Absolute_Course/Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -37,6 +37,8 @@ public class EnemyAI : MonoBehaviour
     private MoveAgent moveAgent;
     //총알 발사흫 제어하는 EnemFire 클래스를 저장할 변수
     private EnemyFire enemyFire;
+    //시야각 및 추적 반경을 제어하는 EnemyFOV 클래스를 저장할 변수
+    private EnemyFOV enemyFOV;
 
     //애니메이터 컨트롤러에 정의한 파라미터의 해시값을 미리 추출
     private readonly int hashMove = Animator.StringToHash("IsMove");
@@ -63,6 +65,8 @@ public class EnemyAI : MonoBehaviour
         moveAgent = GetComponent<MoveAgent>();
         //총알 발사를 제어하는 EnemyFire 클래스를 추출
         enemyFire = GetComponent<EnemyFire>();
+        //시야각 및 추적 반경을 제어하는 EnemyFOV 클래스를 추출
+        enemyFOV = GetComponent<EnemyFOV>();
 
         //코루틴 지연시간 생성
         ws = new WaitForSeconds(0.3f);
@@ -91,6 +95,8 @@ public class EnemyAI : MonoBehaviour
     //적 캐릭터의 상태를 검사하는 코루틴 함수
     IEnumerator CheckState()
     {
+        //오브젝트 풀에 생성 시 다른 스크립트의 초기화를 위해 대기
+        yield return new WaitForSeconds(1.0f);
         //적 캐릭터가 사망하기 전까지 도는 무한루프
         while(!isDie)
         {
@@ -103,7 +109,10 @@ public class EnemyAI : MonoBehaviour
             //공격 사정거리 이내인 경우
             if(dist <= attackDist)
             {
-                state = State.ATTACK;
+                if (enemyFOV.isViewPlayer())
+                    state = State.ATTACK;       //장애물이 없으면 공격 모드
+                else
+                    state = State.TRACE;        //장애물이 있으면 추적 모드
             }//저적 사정거리 이내인 경우
             else if(dist <= traceDist)
             {
